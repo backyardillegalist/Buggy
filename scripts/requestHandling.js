b163ff6ca47a70889e4d944c15059faddbd03df9
backyardillegalist <140549802+backyardillegalist@users.noreply.github.com>
2023-08-04 19:45:49 +0630

    Add files via upload

--- scripts/requestHandling.js
+++ scripts/requestHandling.js
@@ -1,7 +1,14 @@
-const ID = 1;
+let ID = 1;
 const UAs = ["Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36",
 "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
 "Googlebot/2.1 (+http://www.google.com/bot.html)"]
+
+chrome.declarativeNetRequest.getDynamicRules((rules) => {
+  // remove all exsiting rules
+  for(let rule of rules) {
+    chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds: [rule.id]});
+  }
+});
 
 function setHeaders () {
   chrome.storage.sync.set({"set": true}); // set enable state
@@ -23,15 +30,20 @@ function setHeaders () {
           "resourceTypes": [ "main_frame" ]
       }
     }]
+  }, () => {
+    chrome.declarativeNetRequest.getDynamicRules((rules) => {
+      ID = rules[rules.length-1].id; // set id to newly added rule id because sometime browser might automically change id if 1 already exist
+
+    });
   });
 };
 
 
-
 chrome.runtime.onMessage.addListener((data) => {
+
   let msg = data.message[0].set;
 
-  if(msg) {
+  if(msg == true) {
     // remove old rules and create new
     chrome.declarativeNetRequest.updateDynamicRules({
       removeRuleIds: [ID]
@@ -40,10 +52,11 @@ chrome.runtime.onMessage.addListener((data) => {
 
     // disable
     chrome.storage.sync.set({"set": false}); // set disable state
-    chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds: [ID]})
+    chrome.declarativeNetRequest.updateDynamicRules({removeRuleIds: [ID]});
   }
 });
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({"set": false}); // set disable state
+
 });
